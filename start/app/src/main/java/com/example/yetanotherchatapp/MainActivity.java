928f5b0650156d4243a086fe4a26d2ea5d578755
Rich Gowman <rgowman@google.com>
2017-10-14 00:00:35 -0400

    Change app into a chat app, with placeholders.
    No actual chatting is possible; placeholders are used to represent
    messages instead.

--- start/app/src/main/java/com/example/yetanotherchatapp/MainActivity.java
+++ start/app/src/main/java/com/example/yetanotherchatapp/MainActivity.java
@@ -2,6 +2,18 @@ package com.example.yetanotherchatapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +21,93 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText chatMessageEntry = (EditText)findViewById(R.id.chat_message_entry);
+        chatMessageEntry.getText();
+        chatMessageEntry.setOnKeyListener(new View.OnKeyListener() {
+                @Override
+                public boolean onKey(View v, int keyCode, KeyEvent event) {
+                    if ((event.getAction() == KeyEvent.ACTION_DOWN)
+                            && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                        Toast.makeText(
+                                chatMessageEntry.getContext(),
+                                "Message is: " + chatMessageEntry.getText(),
+                                Toast.LENGTH_SHORT).show();
+
+                        return true;
+                    }
+                    return false;
+                }
+            });
+
+        RecyclerView messagesList = (RecyclerView)findViewById(R.id.messages_view);
+        messagesList.setLayoutManager(new LinearLayoutManager(this));
+
+        List<String> placeholderStrings = new ArrayList<>();
+        for (int i=0; i<1000; i++) {
+            placeholderStrings.add("item" + i);
+        }
+        messagesList.setAdapter(new ChatViewAdapter(placeholderStrings));
+    }
+
+    private static class ChatViewAdapter extends RecyclerView.Adapter<ChatViewAdapter.ViewHolder>
+    {
+        private final List<String> placeholderStrings;
+
+        ChatViewAdapter(List<String> placeholderStrings) {
+            this.placeholderStrings = placeholderStrings;
+        }
+
+        /**
+         * Create new views, as necessary.
+         *
+         * The layout manager calls this function whenever it needs a new view.
+         * These views will be reused. No need to set the content; the layout
+         * manager will do that by calling onBindViewHolder().
+         */
+        @Override
+        public ChatViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            ViewGroup layout = (ViewGroup) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item, parent, false);
+            return new ChatViewAdapter.ViewHolder(layout);
+        }
+
+        /**
+         * Replace the contents of a view.
+         *
+         * The layout manager calls this function whenever it wants to display
+         * a view with (potentially) different contents.
+         */
+        @Override
+        public void onBindViewHolder(ChatViewAdapter.ViewHolder viewHolder, int position) {
+            viewHolder.setTextContents(placeholderStrings.get(position));
+        }
+
+        /**
+         * Calculate the size of the dataset.
+         *
+         * The layout manager calls this function whenever it needs to
+         * determine how many items are in the dataset.
+         */
+        @Override
+        public int getItemCount() {
+            return placeholderStrings.size();
+        }
+
+        /**
+         * Represents a view of each data item in our list.
+         */
+        static class ViewHolder extends RecyclerView.ViewHolder {
+            private final TextView listItemTextView;
+
+            ViewHolder(ViewGroup layout) {
+                super(layout);
+                listItemTextView = layout.findViewById(R.id.list_item_text_view);
+            }
+
+            void setTextContents(String text) {
+                listItemTextView.setText(text);
+            }
+        }
     }
 }
